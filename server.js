b9e60102965bddcc4158a85d8fb1d0a92e7fcf1c
Nicholas Altunian <naaltunian@icloud.com>
2022-12-20 17:50:12 -0500

    added database

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const vehicleRoutes = require('./controllers/vehicle')
 require('dotenv').config()
+
+
 const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use('/vehicle', vehicleRoutes)
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
@@ -8,7 +17,12 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.send('about page')
-  })
+})
+
+// database connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
 
 const PORT = process.env.PORT
 
